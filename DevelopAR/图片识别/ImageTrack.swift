33973e35c8d414389535a66e5d14b3d340a7f943
Ht <htreturntrue@163.com>
2019-06-28 13:55:31 +0800

    图片识别

---
+++ DevelopAR/图片识别/ImageTrack.swift
@@ -0,0 +1,50 @@
+//
+//  ImageTrack.swift
+//  DevelopAR
+//
+//  Created by Mac on 2019/6/28.
+//  Copyright © 2019 Ht. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ImageTrack: ARBasicVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let config = ARImageTrackingConfiguration()
+        
+        // 下面参数中的groupName是Assets.xcassets资源管理中的AR资源文件夹名字
+        // 默认是没有的，点击Assets.xcassets文件，new 一个 AR Resource Group
+        guard let referenceImgs = ARReferenceImage.referenceImages(inGroupNamed: "ImageTrackSources", bundle: nil) else { assert(false, "未找到识别图资源") }
+        
+        // 也可以通过下面的方式生成单个 ARReferenceImage，然后设置
+        //        ARReferenceImage(<#T##image: CGImage##CGImage#>, orientation: <#T##CGImagePropertyOrientation#>, physicalWidth: <#T##CGFloat#>)
+        
+        config.trackingImages = referenceImgs
+        
+        scnView.session.run(config)
+        
+        scnView.scene = SCNScene()
+        
+        scnView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
+extension ImageTrack: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let imgAnchor = anchor as? ARImageAnchor {
+            print("图片大小: \(imgAnchor.referenceImage.physicalSize)")
+            let plane = SCNPlane(width: imgAnchor.referenceImage.physicalSize.width, height: imgAnchor.referenceImage.physicalSize.height)
+            plane.materials.first?.diffuse.contents = UIColor.red
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.transform = SCNMatrix4MakeRotation(-(Float.pi / 2), 1, 0, 0)
+            node.addChildNode(planeNode)
+        } else {
+            print("不是图片")
+        }
+    }
+}
