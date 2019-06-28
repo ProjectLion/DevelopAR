e20ac49ec71effd10aeb0d63b31267916295605c
Ht <htreturntrue@163.com>
2019-06-28 14:56:30 +0800

    添加物体识别

---
+++ DevelopAR/物体识别/ObjectTrack.swift
@@ -0,0 +1,58 @@
+//
+//  ObjectTrack.swift
+//  DevelopAR
+//
+//  Created by Mac on 2019/6/28.
+//  Copyright © 2019 Ht. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+/// 在测物体识别的时候请替换 or 添加你自己的识别体资源文件。在Assets.xcassets资源组中的ObjectTrakSources中
+/// 因为我demo里的物体你没有
+class ObjectTrack: ARBasicVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let config = ARWorldTrackingConfiguration()
+        
+        // 下面参数中的groupName是Assets.xcassets资源管理中的AR资源文件夹名字
+        // 默认是没有的，点击Assets.xcassets文件，new 一个 AR Resource Group
+        guard let referenceImgs = ARReferenceObject.referenceObjects(inGroupNamed: "ObjectTrackSources", bundle: nil) else { assert(false, "未找到物体识别资源") }
+        
+        // 也可以通过下面的方式生成单个 ARReferenceObject，然后设置
+//        ARReferenceObject(archiveURL: <#T##URL#>) throw
+        
+        config.detectionObjects = referenceImgs
+        // 可以在识别物体的同时增加平面识别，只需在代理方法里根据回调给的锚点类型判断
+        config.planeDetection = .horizontal
+        
+        scnView.session.run(config)
+        
+        scnView.scene = SCNScene()
+        
+        scnView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ObjectTrack: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let objAnchor = anchor as? ARObjectAnchor {
+            print("识别到名为: \(objAnchor.name ?? "空空空") 的物体")
+            let objSize = objAnchor.referenceObject.extent
+            // 生成一个box
+            let plane = SCNBox(width: CGFloat(objSize.x), height: CGFloat(objSize.y), length: CGFloat(objSize.z), chamferRadius: 0)
+            plane.materials.first?.diffuse.contents = UIColor.red
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            node.addChildNode(planeNode)
+        } else {
+            print("不是物体")
+        }
+    }
+}
